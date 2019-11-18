8e22a30c08a5ced5802fa8c8a8625e03bd4e9ad3
18045945 <18045945@myrp.edu.sg>
2019-11-18 15:56:36 +0800

    Last commit

--- Lesson05quiz.js
+++ Lesson05quiz.js
@@ -10,10 +10,11 @@ const Lesson05quiz = () => {
 
     display.push(<Text key={PET_DATA.name}>{str + PET_DATA.name}</Text>);
     display.push(<Text key={PET_DATA.age}>{str + PET_DATA.age}</Text>);
-    display.push(<Image source ={PET_DATA.image}/>);
+    display.push(<Image source={PET_DATA.image}/>);
     display.push(<Text key={OWNER_DATA.name}>{str + OWNER_DATA.name}</Text>)
     display.push(<Text key={OWNER_DATA.contact}>{str + OWNER_DATA.contact}</Text>)
     display.push(<Text key={OWNER_DATA.membership}>{str + OWNER_DATA.membership}</Text>)
+
     return (
         <View>
             {display}
